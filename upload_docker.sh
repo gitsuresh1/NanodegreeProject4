01c1b13318af84bb192d3db241f6ea4a417ddca2
Suresh B <send2suresh@gmail.com>
2021-12-22 23:44:18 +0530

    Updates

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=nanodegreeproject4:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
 
 # Step 3:
 # Push image to a docker repository
+docker tag nanodegreeproject4:latest dockerjsb/nanodegreeproimg:latest
+docker push dockerjsb/nanodegreeproimg:latest
