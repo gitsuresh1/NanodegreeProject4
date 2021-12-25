5a9f8ff35c6ccf41bb89b219c1d500944a0d03ff
EC2 Default User <ec2-user@ip-172-31-47-27.ec2.internal>
2021-12-25 18:47:31 +0000

    kubernetes output

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
 # This tags and uploads an image to Docker Hub
-
+echo "---------STARTING MINIKUBE -------------"
+minikube start
+echo "--------- MINIKUBE STARTED-------------"
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
-
+dockerpath=dockerjsb/nanodegreeproimg:latest
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+echo "---------DEPLOYING DOCKER IMAGE  -------------"
+kubectl run nanodegreeproimg --image=$dockerpath --port=8080
+echo "---------DOCKER IMAGE DEPLOYED SUCCESSFULLY -------------"
 # Step 3:
 # List kubernetes pods
+echo "---------LISTING THE PODS  -------------"
+kubectl get pods
+echo "--------------------------- -------------"
+
 
 # Step 4:
 # Forward the container port to a host
+echo "---------FORWARDING THE PORTS HOST:CONTAINER PORT MAPPING  -------------"
+kubectl port-forward --address 0.0.0.0 pod/nanodegreeproimg 8080:80
 
